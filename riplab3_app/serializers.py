bef2496f04d0460d32fb06e3d3281c93f8d21822
Denis <DenBond2002@mail.ru>
2022-11-20 20:13:00 +0300

    Second commit

--- riplab3_app/serializers.py
+++ riplab3_app/serializers.py
@@ -5,17 +5,17 @@ from rest_framework import serializers
 
 
 class QuestSerializer(serializers.ModelSerializer):
-    # organizer_name = serializers.CharField(source='organizer.organizer_name')
-    # genre_name = serializers.CharField(source='genre.genre_name')
+    organizer_name = serializers.CharField(source='organizer.organizer_name')
+    genre_name = serializers.CharField(source='genre.genre_name')
 
     class Meta:
         # Модель, которую мы сериализуем
         model = Quest
         # Поля, которые мы сериализуем
-        # fields = ["id_quest", "quest_name", "organizer_name", "address", "genre_name", "capacity", "description",
-        #           "preview_pic", "price"]
-        fields = ["id_quest", "quest_name", "organizer", "address", "genre", "capacity", "description",
+        fields = ["id_quest", "quest_name", "organizer_name", "address", "genre_name", "capacity", "description",
                   "preview_pic", "price"]
+        # fields = ["id_quest", "quest_name", "organizer", "address", "genre", "capacity", "description",
+        #           "preview_pic", "price"]
         # fields = "__all__"
 
 
